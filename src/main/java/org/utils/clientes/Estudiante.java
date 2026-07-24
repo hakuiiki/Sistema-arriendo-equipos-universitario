f9fbb5d5add7510057c557081ef1ecb5551dd8f5
hakuiiki <nacho.placenciap@gmail.com>
2026-07-23 20:35:31 -0400

    feat: Completar constructor de clase hija Estudiante

--- src/main/java/org/utils/clientes/Estudiante.java
+++ src/main/java/org/utils/clientes/Estudiante.java
@@ -2,8 +2,16 @@ package org.utils.clientes;
 
 public class Estudiante extends Clientes{
 
-    public Estudiante(int id, String nombre, int saldo, boolean puedeArrendar, int garantia){
-        super(id, nombre,saldo, true,garantia);
+    private int matricula;
+    private String carrera;
+
+    public Estudiante(int id, String nombre, String correo, int saldoCliente, boolean puedeArrendar, int garantia,
+                      int matricula, String carrera){
+
+        super(id, nombre, correo, saldoCliente, puedeArrendar, garantia);
+
+        this.matricula = matricula;
+        this.carrera = carrera;
     }
     @Override
     public double getPorcentajeDescuento() {
